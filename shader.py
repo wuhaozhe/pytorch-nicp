a526ae89f4c65cf78c7bd7d7ff4bd7ad77dba82a
wuhaozhe <wuhz19@mails.tsinghua.edu.cn>
2021-10-29 14:33:53 +0800

    add code head to all files

--- shader.py
+++ shader.py
@@ -1,3 +1,9 @@
+# Copyright 2021 by Haozhe Wu, Tsinghua University, Department of Computer Science and Technology.
+# All rights reserved.
+# This file is part of the pytorch-nicp,
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
 import torch
 import torch.nn as nn
 from pytorch3d.renderer.mesh.rasterizer import Fragments
